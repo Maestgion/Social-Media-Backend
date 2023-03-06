be49eaf0f9fe62839758c6b62858df943156ac26
Abhishek Dubey <7abhishek1410@gmail.com>
2023-03-07 00:35:26 +0530

    updates

--- src/app.js
+++ src/app.js
@@ -6,7 +6,16 @@ const dotenv = require("dotenv")
 dotenv.config({path:"../config/config.env"})
 const PORT = process.env.PORT
 require("../db/conn")
+const userRoute = require("../router/users")
+const authRoute = require("../router/auth")
 
+app.use(express.json())
+app.use(helmet)
+app.use(morgan("combined"))
+
+
+app.use("/api/user", userRoute)
+app.use("/api/auth", authRoute)
 
 app.listen(PORT, ()=>{
     console.log("server up")
